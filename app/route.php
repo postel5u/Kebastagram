8b73f2b641b510ee27becdb53fcfa5e000845bd6
Guillaume Launay <g.launay96@gmail.com>
2016-10-17 16:29:03 +0200

    Create Homepage

--- app/route.php
+++ app/route.php
@@ -6,4 +6,8 @@
 
 $app->get('/', 'App\Controllers\HomeController:dispatch')->setName('homepage');
 
-$app->get('/users', 'App\Controllers\UserController:dispatch')->setName('userpage');
+$app->get('/login', 'App\Controllers\UserController:login')->setName('login');
+
+$app->get('/signin', 'App\Controllers\UserController:signin')->setName('signin');
+
+$app->get('/about', 'App\Controllers\HomeController:about')->setName('about');
