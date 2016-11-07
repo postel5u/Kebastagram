46e99842b89cbb1b1eca7202e615b2db72822755
Guillaume Launay <g.launay96@gmail.com>
2016-11-07 15:19:01 +0100

    timeline ok

---
+++ app/src/Models/Follows.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gl55
+ * Date: 05/11/2016
+ * Time: 13:44
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Follows extends Model
+{
+    protected $table = "follows";
+    protected $primaryKey ="id_user";
+    public $timestamps = false;
+}
