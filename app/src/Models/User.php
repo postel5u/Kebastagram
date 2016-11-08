ed0a0b5d8fb3d603a694580f8afacf1291429f4d
debian <alexandre.pierre5@etu.univ-lorraine.fr>
2016-11-08 17:12:21 +0100

    solving bugs + resize image

--- app/src/Models/User.php
+++ app/src/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = "users";
-    protected $primaryKey ="id";
+    protected $primaryKey ="uniqid";
+    public $incrementing = false;
     public $timestamps = false;
 }
