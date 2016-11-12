e432a80040c3330afd8684d5bc2d79820ed6d3ef
Guillaume Launay <g.launay96@gmail.com>
2016-11-12 22:52:09 +0100

    comment date

--- app/src/Controllers/UserController.php
+++ app/src/Controllers/UserController.php
@@ -636,7 +636,7 @@ final class UserController
 
     function deletepic(Request $request, Response $response, $args){
       \App\Models\Pictures::find($_GET['id'])->delete();
-      return $this->view->render($response,'profil.twig', array(   ));
+        return $response->withRedirect($this->router->pathFor('profil'));
     }
 
     function deletecom (Request $request, Response $response, $args){
